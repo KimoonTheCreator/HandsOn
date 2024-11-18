e81b310e114bc0ec16bd398cfc1ae2dd91bdfa04
KimoonTheCreator <pangaribuanbram@gmail.com>
2024-11-18 19:47:23 +0700

    BACKGROUND LOOOP
    HELL YEAH

--- Assets/Script/Collector.cs
+++ Assets/Script/Collector.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Collecter : MonoBehaviour
-{
+{   private float width = 0f;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        width=GameObject.Find("BG").GetComponent<BoxCollider2D>().size.x;
     }
 
     // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D target)
     {
-        
+        if (target.tag=="BG"|| target.tag== "Ground"){
+            Vector3 temp = target.transform.position;
+            temp.x += width * 13;
+            target.transform.position = temp;
     }
-}
+ }
+}//class
