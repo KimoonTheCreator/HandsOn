d52b348e99d1d953e192917b17547128f18e92aa
Mimir <119404804+M1m1rr@users.noreply.github.com>
2024-11-27 20:36:40 +0700

    add wallGenerator

--- Assets/ObstacleKillerScript.cs
+++ Assets/ObstacleKillerScript.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCamera : MonoBehaviour
+public class ObstacleKillerScript : MonoBehaviour
 {
-    private float speed = 3f;
-
+    public float speed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
