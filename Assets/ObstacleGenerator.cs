d40a2ff5f701078997c35736bbca292094c1d740
Mimir <119404804+M1m1rr@users.noreply.github.com>
2025-01-17 18:55:49 +0700

    Update ObstacleGenerator.cs
    add comments

--- Assets/ObstacleGenerator.cs
+++ Assets/ObstacleGenerator.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class ObstacleGenerator : MonoBehaviour
 {
-    public GameObject Dinding;
-    public float speed = 3f;
-    public float spawnRate = 2;
-    private float timer = 0;
-    public float offset = 3;
+    public GameObject Dinding; //Assign prefab Wall
+    public float speed = 3f; 
+    public float spawnRate = 2; //Spawn rate obstacle (interchangeable)
+    private float timer = 0; //Timer resets when obstacle spawns
+    //public float offset = 3; REDUNDANT (might still need idk)
     // Start is called before the first frame update
     void Start()
     {
@@ -18,25 +18,25 @@ public class ObstacleGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 temp = transform.position;
-        temp.x += speed * Time.deltaTime;
+        Vector3 temp = transform.position; //Spawn position based on time since game start
+        temp.x += speed * Time.deltaTime; 
         transform.position = temp;
 
         if (timer < spawnRate) 
         {
-            timer += Time.deltaTime;
+            timer += Time.deltaTime; 
         }
         else
         {
-            spawnWall();
-            timer = 0;
+            spawnWall(); 
+            timer = 0; //Timer resets when obstacle spawns
         }
     }
 
-    void spawnWall()
+    void spawnWall() //Obstacle spawn function
     {
-        float lowWall = transform.position.y - 2;
-        float highWall = transform.position.y + 2;
-        Instantiate(Dinding, new Vector3(Random.Range(transform.position.x, transform.position.x + Random.Range(6, 12)), Random.Range(lowWall, highWall), 0), transform.rotation);
+        float lowWall = transform.position.y - 2; //Obstacle spawn coordinates in y axis(s) (interchangeable)
+        float highWall = transform.position.y + 2; 
+        Instantiate(Dinding, new Vector3(Random.Range(transform.position.x, transform.position.x + Random.Range(6, 12)), Random.Range(lowWall, highWall), 0), transform.rotation); //Obstacle spawns randomly between low and high state
     }
 }
